29cb25c4b2b5d62f2a5ef950e6fde9d5789c9b59
wxxd-fxrest <fxrest704@gmail.com>
2023-06-08 20:58:33 +0900

    ver3

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -103,17 +103,6 @@ exports.list = async ctx => {
 */
 exports.read = ctx => {
     ctx.body = ctx.state.post;
-    // const {id} = ctx.params;
-    // try {
-    //     const post = await Post.findById(id).exec();
-    //     if(!post) {
-    //         ctx.status = 404 // Not Found 
-    //         return;
-    //     }
-    //     ctx.body = post;
-    // } catch (e) {
-    //     ctx.throw(500, e); 
-    // }
 }; 
 
 /*
